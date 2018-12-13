7ec18ada389d73ef8cfd293d009916a7fc8c7ea4
Heather Barry <heather.barry@heatherbarryml1.infusionsoft.phx>
2018-12-13 10:07:00 -0700

    changes

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(){
     var theForm = document.getElementById('the-form')
-    console.log('the form', theForm)
+    
+    theForm.addEventListener('submit', function(event){
+        
+        //prevents the default page from reloading
+        event.preventDefault()
+
+        //target the new todo input field
+        var newTodoFieldInput = document.getElementById('new-todo-field')
+
+        //store the value of the input field
+        var newTodo = newTodoFieldInput.value
+
+        //create the new list item
+        var newListItem = document.createElement('li')
+
+        //add the todo to the list item text
+        newListItem.innerText = newTodo
+
+        var listOfTodos = document.getElementById('list-of-todos')
+
+        listOfTodos.appendChild(newListItem)
+
+        document.getElementById('the-form').reset()
+    })
 })
